3a6737bf6e83fdb11f60c87ebd868ee3d3d1796e
Matt Gross <mbgross111@gmail.com>
2021-01-25 19:00:54 -0800

    error handling on api calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,29 +37,45 @@ let searchHistoryHandler = function(event) {
 let fetchCurrentWeather = function(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
   fetch(apiUrl).then(function(res) {
-    res.json().then(function(currentWeather) {
-      let cityName = currentWeather.name;
-      // create java script date time object from weather api dt (10 dig num)
-      let dateTime = moment.utc(currentWeather.dt*1000 + currentWeather.timezone*1000);
-      // get temperature kelvin => Fahrenheit
-      let tempF = Math.round((currentWeather.main.temp - 273.15)* 9/5 + 32);
-      let humidity = currentWeather.main.humidity;
-      // get wind speed => MPH
-      let windSpeedMph = Math.round(currentWeather.wind.speed * 2.237);
-      // get lon and lat
-      let lon = currentWeather.coord.lon;
-      let lat = currentWeather.coord.lat;
-      let iconCode = currentWeather.weather[0].icon;
-      let description = currentWeather.weather[0].description;
-      fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`).then(function(res) {
-        res.json().then(function(uvData) {
-          let uvIndex = uvData.value;
-          displayCurrentWeather(cityName, iconCode, description, dateTime, tempF, humidity, windSpeedMph, uvIndex)
-          addSearchItem(cityName);
-        })
-      });
-      fiveDayForecast(lat,lon);
-    })
+    if (res.ok) {
+      res.json().then(function(currentWeather) {
+        let cityName = currentWeather.name;
+        // create java script date time object from weather api dt (10 dig num)
+        let dateTime = moment.utc(currentWeather.dt*1000 + currentWeather.timezone*1000);
+        // get temperature kelvin => Fahrenheit
+        let tempF = Math.round((currentWeather.main.temp - 273.15)* 9/5 + 32);
+        let humidity = currentWeather.main.humidity;
+        // get wind speed => MPH
+        let windSpeedMph = Math.round(currentWeather.wind.speed * 2.237);
+        // get lon and lat
+        let lon = currentWeather.coord.lon;
+        let lat = currentWeather.coord.lat;
+        let iconCode = currentWeather.weather[0].icon;
+        let description = currentWeather.weather[0].description;
+        
+        fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+          .then(function(res) {
+            if(res.ok) {
+              res.json().then(function(uvData) {
+                let uvIndex = uvData.value;
+                displayCurrentWeather(cityName, iconCode, description, dateTime, tempF, humidity, windSpeedMph, uvIndex)
+                addSearchItem(cityName);
+              })
+            } else {
+              alert("Error: " + res.statusText);
+            }
+          })
+          .catch(function(err) {
+            todayContainerEl.innerHTML = `<h4> Unable to retrieve current weather for ${cityName}<h4>`
+          });
+        fiveDayForecast(lat,lon);
+      })
+    } else {
+      alert("Error: " + res.statusText);
+    }
+  })
+  .catch(function(err) {
+    alert("Unable to connect to weather server. Please check connection and try again.")
   })
 };
 
@@ -67,72 +83,79 @@ let fetchCurrentWeather = function(city) {
 let fiveDayForecast = function(lat, lon) {
   apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
   fetch(apiUrl).then(function(res) {
-    res.json().then(function(fiveDayObj) {
-      // CLEAR FORECAST CONTAINER
-      forecastContainerEl.textContent = ""
-      let daily = fiveDayObj.daily;
-      
-      // CREATE H2 
-      let forecastHeaderEl = document.createElement("h2");
-      forecastHeaderEl.textContent = "5-Day Forecast: ";
-      forecastContainerEl.appendChild(forecastHeaderEl);
-      
-      // start forecast loop tomorrow if after 4pm
-      if (moment().get('hour') >= 16) {
-        start = 1
-      } else {
-        start = 0
-      };
-
-      for (let i = start; i < start + 5; i++) {
-        // CREATE MY VARIABLES 
-        let iconCode = daily[i].weather[0].icon;
-        let dateTime = moment.utc(daily[i].sunrise*1000 + fiveDayObj.timezone_offset*1000);
-        let hiF = Math.round((daily[i].temp.max - 273.15)* 9/5 + 32);
-        let loF = Math.round((daily[i].temp.min - 273.15)* 9/5 + 32);
-        let hum = daily[i].humidity
-
-        // CREATE CARD
-        let cardEl = document.createElement("div");
-        cardEl.classList = "card text-dark bg-light";
-        // Create card header
-        let cardHeaderEl = document.createElement("h3");
-        cardHeaderEl.classList = "card-header px-0 text-center";
-        cardHeaderEl.textContent = dateTime.format("ddd");
+    if(res.ok){
+      res.json().then(function(fiveDayObj) {
+        // CLEAR FORECAST CONTAINER
+        forecastContainerEl.textContent = ""
+        let daily = fiveDayObj.daily;
         
-        // Create card-body
-        let cardBodyEl = document.createElement("div");
-        cardBodyEl.classList = "card-body";
-        let cardIconEl = document.createElement("img");
-        // icon
-        cardIconEl.classList = "card-img"
-        cardIconEl.src = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
-        cardIconEl.alt = daily[i].weather[0].description;
-        // hi
-        hiFEl = document.createElement("p");
-        hiFEl.classList = "card-text text-danger"
-        hiFEl.textContent = `Hi: ${hiF}°F`
-        // lo
-        loFEl = document.createElement("p");
-        loFEl.classList = "card-text text-primary"
-        loFEl.textContent = `Lo: ${loF}°F`
-        // hum
-        humEl = document.createElement("p");
-        humEl.classList = "card-text text-warning"
-        humEl.textContent = `Humidity: ${hum}%`
-        // APPEND TO DOM
-        // append weatherInfo to card body
-        cardBodyEl.appendChild(cardIconEl);
-        cardBodyEl.appendChild(hiFEl);
-        cardBodyEl.appendChild(loFEl);
-        cardBodyEl.appendChild(humEl);
-        // append header and body to card
-        cardEl.appendChild(cardHeaderEl);
-        cardEl.appendChild(cardBodyEl);
-        // append whole card to card deck
-        forecastContainerEl.appendChild(cardEl);
-      }
-    })
+        // CREATE H2 
+        let forecastHeaderEl = document.createElement("h2");
+        forecastHeaderEl.textContent = "5-Day Forecast: ";
+        forecastContainerEl.appendChild(forecastHeaderEl);
+        
+        // start forecast loop tomorrow if after 4pm
+        if (moment().get('hour') >= 16) {
+          start = 1
+        } else {
+          start = 0
+        };
+
+        for (let i = start; i < start + 5; i++) {
+          // CREATE MY VARIABLES 
+          let iconCode = daily[i].weather[0].icon;
+          let dateTime = moment.utc(daily[i].sunrise*1000 + fiveDayObj.timezone_offset*1000);
+          let hiF = Math.round((daily[i].temp.max - 273.15)* 9/5 + 32);
+          let loF = Math.round((daily[i].temp.min - 273.15)* 9/5 + 32);
+          let hum = daily[i].humidity
+
+          // CREATE CARD
+          let cardEl = document.createElement("div");
+          cardEl.classList = "card text-dark bg-light";
+          // Create card header
+          let cardHeaderEl = document.createElement("h3");
+          cardHeaderEl.classList = "card-header px-0 text-center";
+          cardHeaderEl.textContent = dateTime.format("ddd");
+          
+          // Create card-body
+          let cardBodyEl = document.createElement("div");
+          cardBodyEl.classList = "card-body";
+          let cardIconEl = document.createElement("img");
+          // icon
+          cardIconEl.classList = "card-img"
+          cardIconEl.src = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
+          cardIconEl.alt = daily[i].weather[0].description;
+          // hi
+          hiFEl = document.createElement("p");
+          hiFEl.classList = "card-text text-danger"
+          hiFEl.textContent = `Hi: ${hiF}°F`
+          // lo
+          loFEl = document.createElement("p");
+          loFEl.classList = "card-text text-primary"
+          loFEl.textContent = `Lo: ${loF}°F`
+          // hum
+          humEl = document.createElement("p");
+          humEl.classList = "card-text text-warning"
+          humEl.textContent = `Humidity: ${hum}%`
+          // APPEND TO DOM
+          // append weatherInfo to card body
+          cardBodyEl.appendChild(cardIconEl);
+          cardBodyEl.appendChild(hiFEl);
+          cardBodyEl.appendChild(loFEl);
+          cardBodyEl.appendChild(humEl);
+          // append header and body to card
+          cardEl.appendChild(cardHeaderEl);
+          cardEl.appendChild(cardBodyEl);
+          // append whole card to card deck
+          forecastContainerEl.appendChild(cardEl);
+        }
+      })
+    } else {
+      errorText = "Error: " + response.statusText
+    }
+  })
+  .catch(function(err) {
+    alert(`Unable to retrieve 5-Day Forecast`);
   });
 };
 
